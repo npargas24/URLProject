de1bf424d45293c08eb87a4daab3874b9906ec95
npargas24 <114077181+npargas24@users.noreply.github.com>
2025-03-02 22:40:16 -0500

    added another test outline

--- tests.py
+++ tests.py
@@ -1,10 +1,13 @@
-import URL_Proj_Nat_Backup as Url
+import URL_Proj_Nat as Url
 import unittest
 
 class UrlTest(unittest.TestCase):
     def test(self):
         url_obj = Url.UrlHandler("https://www.google.com/search?q=how+to+impo&oq=how+to+impo&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIKCAEQABixAxiABDIHCAIQABiABDIKCAMQABixAxiABDIKCAQQABixAxiABDIHCAUQABiABDIKCAYQABixAxiABDIHCAcQABiABDIHCAgQABiABDIHCAkQABiABNIBCTcwODgwajBqN6gCCLACAfEFHincNqIHq3w&sourceid=chrome&ie=UTF-8")
+        assert url_obj.valid == True
 
+        url_obj2 = Url.UrlHandler('https://www.google.com/search?q=how+many+episodes+season+2+severance&oq=how+many+episodes+season+2+se&gs_lcrp=EgZjaHJvbWUqDQgAEAAYgwEYsQMYgAQyDQgAEAAYgwEYsQMYgAQyBggBEEUYOTIHCAIQABiABDIHCAMQABiABDIHCAQQABiABDIICAUQABgWGB4yCAgGEAAYFhgeMggIBxAAGBYYHjIICAgQABgWGB4yCAgJEAAYFhge0gEINTc5MWowajmoAgCwAgE&sourceid=chrome&ie=UTF-8')
+        assert url_obj2.valid == True
 
 url_test = UrlTest()
 url_test.test()
